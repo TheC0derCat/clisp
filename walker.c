0c428a603f0fe0f9c9e31d1d1215dc2c2599fc51
Silly Cat <CatC0der@proton.me>
2026-08-21 20:24:41 -0700

    multiple operand handling added

--- walker.c
+++ walker.c
@@ -2,10 +2,34 @@ int walk(struct astnode node){
 	int value = 0;
 	switch(node.tok.type){
 		case NUM: value = node.tok.data.num; break;
-		case ADD: value = walk(node.branchs[0]) + walk(node.branchs[1]); break;
-		case SUB: value = walk(node.branchs[0]) - walk(node.branchs[1]); break;
-		case MUL: value = walk(node.branchs[0]) * walk(node.branchs[1]); break;
-		case DIV: value = walk(node.branchs[0]) / walk(node.branchs[1]); break;
+		case ADD:
+			  for(int i = 0; i < node.branch_count; i++){
+				  value += walk(node.branchs[i]); 
+			  }
+			  break;
+		case SUB:
+			  value = walk(node.branchs[0]); 
+			  for(int i = 1; i < node.branch_count; i++){
+				  value -= walk(node.branchs[i]); 
+			  }
+			  break;
+		case MUL:
+			  value = walk(node.branchs[0]); 
+			  for(int i = 1; i < node.branch_count; i++){
+				  value *= walk(node.branchs[i]); 
+			  }
+			  break;
+		case DIV:
+			  value = walk(node.branchs[0]); 
+			  for(int i = 1; i < node.branch_count; i++){
+				  int thing = walk(node.branchs[i]); 
+				  if(thing == 0){
+					  puts("you cant divide by zero you silly goober");
+					  exit(1);
+				  }
+				  value /= thing; 
+			  }
+			  break;
 		default: puts("unexpected node");
 	}
 	return value;
